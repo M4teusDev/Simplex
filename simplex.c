99b9c06c5048ad0f9eb8643b84adf14cca271368
Mateus Prado <mateus.silvaprado@gmail.com>
2020-10-03 15:26:59 -0300

    Simplex_v2

--- simplex.c
+++ simplex.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int  readMatrix(char *path, int **data);
 void aloca(int **p, int tam);
 int  qtdVariaveis(char *path);
 int  qtdLinhas(char *path);
 void inicializa_matriz(int lin, int col, float m[lin][col]);
+void monta_matriz(int lin, int col, float m[lin][col], int variaveis, int *p);
+void adiciona_folgas(int lin,int col,float m[lin][col]);
+void exibe_matriz(int lin,int col,float m[lin][col]);
 
 int main ( void )
 {
   char file[100] = "exemplo.txt";
-  int *m = NULL, i = 0, tam_aloc, variaveis = 0, colunas = 0, linhas = 0, j = 0;
+  int *m = NULL, i = 0, tam_aloc, variaveis = 0, colunas = 0, linhas = 0, j = 0, restricoes = 0;
   tam_aloc  = readMatrix(file, &m);
   variaveis = qtdVariaveis(file);
+  restricoes = variaveis + 1;
   colunas = variaveis * 2 + 2;
   linhas    = variaveis + 1;
 
   float matriz[linhas + 1][colunas];
 
-  inicializa_matriz(linhas + 1, colunas, matriz);
+   inicializa_matriz(linhas + 1, colunas, matriz);
+   monta_matriz(linhas + 1, colunas, matriz, variaveis, m);
+
+   adiciona_folgas(linhas + 1, colunas, matriz);
+   exibe_matriz(linhas + 1, colunas, matriz);
 }
 
 int readMatrix(char *path, int **data)
@@ -29,7 +38,7 @@ int readMatrix(char *path, int **data)
 
     if (file == NULL) {
         fprintf(stderr, "error: while trying to open `%s' for reading\n", path);
-        return; //
+        return 0; //
     }
     
     while (fscanf(file, "%d ", &aux) == 1) {
@@ -61,7 +70,7 @@ int qtdVariaveis(char *path)
   fread (&c, sizeof(char), 1, file);
   result = fgets(linha, 100, file);
 
-  for(i = 0; i < strlen(linha); i++)
+  for(i = 0; i < (strlen(linha)); i++)
     if( *(result + i) == ' ')
       cont++;
     
@@ -72,19 +81,55 @@ int qtdVariaveis(char *path)
 void inicializa_matriz(int lin, int col, float m[lin][col]) 
 {
   int i = 0, j = 0;
-  for(i = 0; i < lin; i++) {
-    for(j = 0; j < col; j++){
+  for(i = 0; i < lin; i++)
+    for(j = 0; j < col; j++)
       m[i][j] = 0.0;
-    }
-    printf("\n");
-  }
-
 }
 
-// void monta_matriz(int lin, int col, float m[lin][col], int variaveis, int *p, int tam_aloc)
-// {
-//   for(i = variaveis; i < tam_aloc; i++)
-//   {
-//     printf("oi");
-//   }
-// }
+void monta_matriz(int lin, int col, float m[lin][col], int variaveis, int *p)
+{
+    int  i = 0, j = 0, k = 0;
+
+    for(i = 0; i < lin - 1; i++)
+    {
+        for(j = 0; j < col; j++)
+        {
+            if(j < variaveis) 
+            {
+                m[i][j] = *(p + k + variaveis);
+                k++;
+            }
+            else if(j == col - 1)
+            {
+                m[i][j] = *(p + k + variaveis);
+                k++;
+            }
+        }
+        printf("\n");   
+    }
+}
+
+void adiciona_folgas(int lin,int col,float m[lin][col]) 
+{
+    int  i = 0, j = 0;
+
+    for( i = 0; i < lin - 1; i++) 
+        for( j = 0; j < col; j++)
+            if(j - i == 2)
+                m[i][j] = 1.0;
+}
+
+void exibe_matriz(int lin,int col,float m[lin][col])
+{
+    int  i = 0, j = 0;
+
+    for( i = 0; i < lin; i++)
+    {
+        for( j = 0; j < col; j++) 
+        {
+            printf("%.0f\t", m[i][j]);
+        }
+        printf("\n");        
+    }
+        
+}
